f7b8f8f512967a6957b357b4be03eebfef4030d2
marmai-jma <johane.marmai@gmail.com>
2019-12-30 09:12:27 +0100

    avant test

--- src/app/components/review-list/review-list.component.ts
+++ src/app/components/review-list/review-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ReviewDto } from 'src/app/shared-data/review-dto';
 
 @Component({
   selector: 'app-review-list',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review-list.component.scss']
 })
 export class ReviewListComponent implements OnInit {
-
+  @Input() mediaId: string;
+  @Input() reviews: ReviewDto[];
   constructor() { }
 
   ngOnInit() {
+    console.log(this.reviews[0].comment)
   }
 
 }
